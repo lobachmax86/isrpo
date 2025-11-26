1cbeae02309e7c15355a1253d234673c0ddf63fc
lobachmax86 <lobachmax86@gmail.com>
2025-11-27 01:48:42 +0300

    Добавил две лабораторные работы: Lab1 и Lab2

---
+++ Lab 1/Lab1/Lab1/Lab1.cpp
@@ -0,0 +1,61 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <locale>
+
+#define UNLEN 256
+#define MAX_COMPUTERNAME_LENGTH 15
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    ULONGLONG ticks = GetTickCount64();
+    std::cout << "Система работает уже: " << ticks << " миллисекунд." << std::endl;
+
+    ULONGLONG total_seconds = ticks / 1000;
+
+    ULONGLONG total_minutes = total_seconds / 60;
+    ULONGLONG seconds = total_seconds % 60;
+
+    ULONGLONG total_hours = total_minutes / 60;
+    ULONGLONG minutes = total_minutes % 60;
+
+    ULONGLONG days = total_hours / 24;
+    ULONGLONG hours = total_hours % 24;
+
+    std::cout << "Форматированное время: ";
+    std::cout << days << "д. " << hours << "ч. " << minutes << "м. " << seconds 
+        << "с. " << std::endl;
+
+    wchar_t computerName[MAX_COMPUTERNAME_LENGTH + 1] = { 0 };
+    wchar_t userName[UNLEN + 1] = { 0 };
+    DWORD size = MAX_COMPUTERNAME_LENGTH + 1;
+
+    // Получаем данные
+    GetComputerNameW(computerName, &size);
+    size = UNLEN + 1;
+    GetUserNameW(userName, &size);
+
+    // Выводим в консоль
+    std::wcout << L"\nИмя компьютера: " << computerName << std::endl;
+    std::wcout << L"Имя пользователя: " << userName << std::endl;
+
+
+    // Получаем текущее системное время
+    SYSTEMTIME st;
+    GetLocalTime(&st); // Получаем локальное время
+
+    // Форматируем строку с датой и временем
+    wchar_t buffer[256];
+    swprintf(buffer, sizeof(buffer) / sizeof(buffer[0]),
+        L"Текущая дата и время:\n"
+        L"%02d.%02d.%04d %02d:%02d:%02d",
+        st.wDay, st.wMonth, st.wYear,
+        st.wHour, st.wMinute, st.wSecond);
+
+    // Выводим в MessageBox
+    MessageBoxW(NULL, buffer, L"Текущее время", MB_OK | MB_ICONINFORMATION);
+
+    return 0;
+
+}
+
---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,82 @@
+﻿#include <iostream>
+#include <cctype>
+#include <cwctype>
+#include <windows.h>
+
+// ansi
+void mystrlwr(char* s) {
+    for (; *s; s++) *s = std::tolower((unsigned char)*s);
+}
+
+char* mystrrchr(const char* s, int ch) {
+    const char* last = nullptr;
+    for (; *s; s++) if (*s == ch) last = s;
+    return (char*)last;
+}
+
+BOOL isstringsame(const char* a, const char* b) {
+    for (; *a && *b; a++, b++)
+        if (std::tolower((unsigned char)*a) != std::tolower((unsigned char)*b))
+            return FALSE;
+    return (*a == '\0' && *b == '\0');
+}
+
+//unicode
+void mystrlwr(wchar_t* s) {
+    for (; *s; s++) *s = std::towlower(*s);
+}
+
+wchar_t* mystrrchr(const wchar_t* s, wchar_t ch) {
+    const wchar_t* last = nullptr;
+    for (; *s; s++) if (*s == ch) last = s;
+    return (wchar_t*)last;
+}
+
+BOOL isstringsame(const wchar_t* a, const wchar_t* b) {
+    for (; *a && *b; a++, b++)
+        if (std::towlower(*a) != std::towlower(*b))
+            return FALSE;
+    return (*a == L'\0' && *b == L'\0');
+}
+
+int main() {
+    setlocale(LC_ALL, "RUSSIAN");
+
+
+    std::cout << "ANSI\n\n";
+
+    char str1[50] = "HeLLo WoRLD";
+    std::cout << "Исходная строка: " << str1 << "\n";
+    mystrlwr(str1);
+    std::cout << "После mystrlwr:   " << str1 << "\n\n";
+
+    const char* path = "C:/folder/subfolder/file.txt";
+    std::cout << "Строка пути: " << path << "\n";
+    char* last = mystrrchr(path, '/');
+    std::cout << "Последний '/': " << (last ? last : "не найден") << "\n\n";
+
+    std::cout << "Сравнение \"Hello\" и \"heLLo\": "
+        << (isstringsame("Hello", "heLLo") ? "совпадают" : "разные") << "\n";
+    std::cout << "Сравнение \"ABC\" и \"ABD\": "
+        << (isstringsame("ABC", "ABD") ? "совпадают" : "разные") << "\n\n";
+
+
+    std::wcout << L"\nUNICODE\n\n";
+
+    wchar_t wstr1[50] = L"HeLLo WoRLD";
+    std::wcout << L"Исходная строка: " << wstr1 << L"\n";
+    mystrlwr(wstr1);
+    std::wcout << L"После mystrlwr:   " << wstr1 << L"\n\n";
+
+    const wchar_t* wpath = L"C:/folder/subfolder/file.txt";
+    std::wcout << L"Строка пути: " << wpath << L"\n";
+    wchar_t* wlast = mystrrchr(wpath, L'/');
+    std::wcout << L"Последний '/': " << (wlast ? wlast : L"не найден") << L"\n\n";
+
+    std::wcout << L"Сравнение \"Hello\" и \"heLLo\": "
+        << (isstringsame(L"Hello", L"heLLo") ? L"совпадают" : L"разные") << L"\n";
+    std::wcout << L"Сравнение \"ABC\" и \"ABD\": "
+        << (isstringsame(L"ABC", L"ABD") ? L"совпадают" : L"разные") << L"\n";
+
+    return 0;
+}
